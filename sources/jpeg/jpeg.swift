71f8d625a516b47af688562894c127963d83e9f6
taylor swift <kelvinsthirteen@gmail.com>
2017-11-06 02:32:09 -0600

    quantization table reader

--- sources/jpeg/jpeg.swift
+++ sources/jpeg/jpeg.swift
@@ -119,6 +119,21 @@ func match(stream:UnsafeMutablePointer<FILE>, against expected:[UInt8]) -> Bool
     return true
 } 
 
+func readUInt8(from stream:UnsafeMutablePointer<FILE>) throws -> UInt8 
+{
+    var uint8:UInt8 = 0 
+    return try withUnsafeMutablePointer(to: &uint8) 
+    {
+        guard fread($0, 1, 1, stream) == 1 
+        else 
+        {
+            throw JPEGReadError.IncompleteMarkerError
+        }
+        
+        return $0.pointee
+    }
+}
+
 func readUInt16(from stream:UnsafeMutablePointer<FILE>) throws -> UInt16 
 {
     var uint16:UInt16 = 0 // allocate this on the stack why not
@@ -134,18 +149,122 @@ func readUInt16(from stream:UnsafeMutablePointer<FILE>) throws -> UInt16
     }
 }
 
-func readUInt8(from stream:UnsafeMutablePointer<FILE>) throws -> UInt8 
+// reads length block and allocates output buffer
+func readMarkerData(from stream:UnsafeMutablePointer<FILE>) 
+    throws -> UnsafeMutableRawBufferPointer
 {
-    var uint8:UInt8 = 0 
-    return try withUnsafeMutablePointer(to: &uint8) 
+    let dest = 
+        UnsafeMutableRawBufferPointer.allocate(count: Int(try readUInt16(from: stream)))
+    
+    guard fread(dest.baseAddress, 1, dest.count, stream) == dest.count 
+    else 
     {
-        guard fread($0, 1, 1, stream) == 1 
-        else 
+        throw JPEGReadError.IncompleteMarkerError
+    } 
+    
+    return dest
+}
+
+private 
+struct Decoder 
+{
+    private 
+    enum QuantizationTable 
+    {
+        case q8 (UnsafeMutablePointer<UInt8>), 
+             q16(UnsafeMutablePointer<UInt16>) 
+        
+        func deallocate() 
         {
-            throw JPEGReadError.IncompleteMarkerError
+            switch self 
+            {
+            case .q8(let buffer):
+                buffer.deinitialize(count: 64)
+                buffer.deallocate(capacity: 64)
+            
+            case .q16(let buffer):
+                buffer.deinitialize(count: 64)
+                buffer.deallocate(capacity: 64)
+            }
+        }
+    }
+    
+    // these must be managed manually or they will leak
+    private 
+    var qtables:(QuantizationTable?, QuantizationTable?, 
+                 QuantizationTable?, QuantizationTable?) = (nil, nil, nil, nil)
+    
+    private mutating 
+    func updateQuantizationTables(from stream:UnsafeMutablePointer<FILE>) throws 
+    {
+        let tableData:UnsafeMutableRawBufferPointer = try readMarkerData(from: stream) 
+        defer 
+        {
+            tableData.deallocate()
         }
         
-        return $0.pointee
+        var i:Int = 0
+        while (i < tableData.count) 
+        {
+            let table:QuantizationTable, 
+                bindingIndex:UInt8 = tableData[i] & 0x0f 
+            
+            guard bindingIndex < 4 
+            else 
+            {
+                throw JPEGReadError.SyntaxError("quantization table has invalid binding index \(bindingIndex) (index must be in 0 ... 3)")
+            }
+            
+            switch tableData[i] & 0xf0 
+            {
+            case 0x00:
+                let cells = UnsafeMutablePointer<UInt8>.allocate(capacity: 64), 
+                    u8    = UnsafePointer<UInt8>((tableData.baseAddress! + 1)
+                            .bindMemory(to: UInt8.self, capacity: 64))
+                cells.initialize(from: u8, count: 64)
+                table = .q8(cells)
+            
+                i += 64 + 1
+            
+            case 0x10: 
+                let cells = UnsafeMutablePointer<UInt16>.allocate(capacity: 64), 
+                    u16   = UnsafePointer<UInt16>((tableData.baseAddress! + 1)
+                            .bindMemory(to: UInt16.self, capacity: 64))
+                for cell:Int in 0 ..< 64 
+                {
+                    (cells + cell).initialize(to: UInt16(bigEndian: u16[cell]))
+                }
+                
+                table = .q16(cells) 
+                
+                i += 128 + 1
+            
+            default:
+                throw JPEGReadError.SyntaxError("quantization table has invalid precision (code: \(tableData[i] >> 4))")
+            }
+            
+            switch bindingIndex 
+            {
+            case 0:
+                qtables.0?.deallocate()
+                qtables.0 = table
+                
+            case 1:
+                qtables.1?.deallocate()
+                qtables.1 = table
+                
+            case 2:
+                qtables.2?.deallocate()
+                qtables.2 = table
+                
+            case 3:
+                qtables.3?.deallocate()
+                qtables.3 = table 
+            
+            default: 
+                fatalError("unreachable")
+            }
+        }
     }
 }
 
